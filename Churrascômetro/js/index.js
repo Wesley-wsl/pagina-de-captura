10b731cd5a5a0d20e58684c70355545787907639
Wesley Westelley <wesleywestelley@gmail.com>
2021-06-27 13:56:55 -0300

    Project finish

--- Churrascômetro/js/index.js
+++ Churrascômetro/js/index.js
@@ -1,6 +1,6 @@
-// Carne - 400 gr por pessoa   + de 6 horas - 650
-// Cerveja - 1200 ml por Pessoa + 6 horas - 2000 ml
-// Refrigerante/agua - 1000 ml por pessoa + 6 horas 1500ml
+// Carne - 400g por pessoa | + de6 horas - 650
+// Cerveja - 1200 ml por Pessoa | +6 horas - 2000 ml
+// Refrigerante/agua - 1000 ml por pessoa |  +6 horas - 1500ml
 
 // Crianças valem por 0,5
 
@@ -13,14 +13,24 @@ const $result = document.querySelector('#result')
 $calc.addEventListener('click', calculo)
 
 function calculo() {
-    let beef = ($adult.value * 0.400).toFixed(2)
-    let beer
-    let water
+    let beef = ($adult.value * 400 + (400 / 2 * $kid.value))
+    let beer = ($adult.value * 1200)
+    let water = ($adult.value * 1000 + (100 / 2 * $kid.value))
 
     if ($time.value > 6) {
-        beef = ($adult.value * 0.650).toFixed(2)
+        beef = ($adult.value * 650) + (650 / 2 * $kid.value)
+        beer = $adult.value * 2000
+        water = ($adult.value * 1500 + (1500 / 2 * $kid.value))
     }
 
-    console.log(beef)
-
+    if ($kid.value == '' || $adult.value == '' || $time.value == '') alert('Preencha todos os campos.')
+    else {
+        $result.innerHTML = `
+        <ul style="margin-top:30px;">
+            <li>${beef / 1000} kg de carne.</li>
+            <li>${Math.ceil(beer / 355)} latas de cerveja.</li>
+            <li>${Math.ceil(beef / 2000)} garrafas de bebidas.</li>
+        </ul>
+    `
+    }
 }
