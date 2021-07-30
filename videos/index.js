6486a17dc7fc34612f7ab37ee3cc8f3801c4f308
Wesley Westelley <wesleywestelley@gmail.com>
2021-07-30 14:50:18 -0300

    Initial Commit

---
+++ videos/index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+
+const videos = [
+    'LbVA2udEec0',
+    'f8WdxqcH1QQ',
+    'htGN1lu8TtA',
+    'PDeTO26fRVQ',
+]
+
+
+app.get('/api/videos', (req, res) => {
+    res.send(videos)
+})
+
+app.use(express.static(path.join(__dirname, 'front/build')))
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'front/build/index.html', function(error) {
+        if(error) {
+            res.status(500).send(error)
+        }
+    }))
+})
+
+
+
+app.listen(3000, () => {
+    console.log('Running')
+})
